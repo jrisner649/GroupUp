91f324f80b28bad78cdc2b12f94c6f9e27f5fed4
jrisner649 <jrisner649@gmail.com>
2025-09-16 10:11:23 -0500

    Fetch for sign up works

--- frontend/js/landing.js
+++ frontend/js/landing.js
@@ -1,14 +1,12 @@
+const URL = 'http://localhost:8000';
 
 // sign up button
 document.querySelector('#btnLandingPageSignUp').addEventListener('click', () => {
     console.log("sign up button clicked");
-    fetch('http://localhost:8000/signUp')
+    fetch('/component/signUp')
     .then(response => response.text())
     .then(html => {
-        document.querySelector('#divLandingPage').classList.add('d-none');
-        document.querySelector('#divSignUp').innerHTML = html;
-        document.querySelector('#divSignUp').classList.remove('d-none');
-
+        document.querySelector('#divMainContent').innerHTML = html
     })
     .catch(error => {
         console.error('Error fetching the sign-up page:', error);
@@ -19,11 +17,15 @@ document.querySelector('#btnLandingPageSignUp').addEventListener('click', () => 
 document.querySelector('#btnLandingPageSignUpBottom').addEventListener('click', () => {
 
     // hide the landing page
-    document.querySelector('#divLandingPage').classList.add('d-none');
+    // document.querySelector('#divLandingPage').classList.add('d-none');
 
-    // unhide the signup page
-    document.querySelector('#divAccountsPage').classList.remove('d-none');
-    document.querySelector('#formRegister').classList.remove('d-none');
+    // fetch the login page
+    fetch(URL + '/component/signUp')
+    .then(response => response.text())
+    .then(html => {
+        document.body.innerHTML = html
+    })
+    // document.querySelector('#formLogin').classList.remove('d-none');
 
 });
 
@@ -33,8 +35,12 @@ document.querySelector('#btnLandingPageLogin').addEventListener('click', () => {
     // hide the landing page
     document.querySelector('#divLandingPage').classList.add('d-none');
 
-    // unhide the signup page
-    document.querySelector('#divAccountsPage').classList.remove('d-none');
+    // fetch the login page
+    fetch(URL + '/component/login')
+    .then(response => response.text())
+    .then(html => {
+        document.body.innerHTML = html
+    })
     document.querySelector('#formLogin').classList.remove('d-none');
 
 });
